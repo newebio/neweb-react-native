78083e39df8d594d6b600adf8d20843ed0f4b433
Vitaly Arkhipov <arvitaly@list.ru>
2018-06-26 23:25:12 +0300

    added exports

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 var Application_1 = require("./lib/Application");
 exports.Application = Application_1.Application;
+__export(require("neweb-react"));
+__export(require("neweb-core"));
--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
 export { Application } from "./lib/Application";
+export * from "neweb-react";
+export * from "neweb-core";
